80be690b72741610221cfe629c3e054620620184
Anup <anupthedev@gmail.com>
2024-11-16 18:48:01 -0500

    Add authentication and signup functionality

---
+++ components/AuthForm.tsx
@@ -0,0 +1,149 @@
+'use client';
+
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { createAccount } from '@/lib/actions/user.actions';
+import OTPModal from './OTPModal';
+
+type AuthFormType = 'sign-in' | 'sign-up';
+
+const authFormSchema = (type: AuthFormType) => {
+	return z.object({
+		email: z.string().email(),
+		fullName:
+			type === 'sign-up' ? z.string().min(2).max(50) : z.string().optional(),
+	});
+};
+
+const AuthForm = ({ type }: { type: AuthFormType }) => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
+	const [accountId, setAccountId] = useState<string | null>(null);
+
+	const formSchema = authFormSchema(type);
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			fullName: '',
+			email: '',
+		},
+	});
+
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+		setIsLoading(true);
+		setErrorMessage(''); // clear any previous error messages
+
+		try {
+			const user = await createAccount(values.fullName || '', values.email);
+			setAccountId(user.accountId);
+		} catch (error) {
+			setErrorMessage('Failed to create account. Please try again.');
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	return (
+		<>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className='auth-form'>
+					<h1 className='form-title'>
+						{type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+					</h1>
+					{type === 'sign-up' && (
+						<FormField
+							control={form.control}
+							name='fullName'
+							render={({ field }) => (
+								<FormItem>
+									<div className='shad-form-item'>
+										<FormLabel className='shad-form-label'>Full Name</FormLabel>
+										<FormControl>
+											<Input
+												placeholder='Enter your full name'
+												className='shad-input'
+												{...field}
+											/>
+										</FormControl>
+									</div>
+									<FormMessage className='shad-form-message' />
+								</FormItem>
+							)}
+						/>
+					)}
+					<FormField
+						control={form.control}
+						name='email'
+						render={({ field }) => (
+							<FormItem>
+								<div className='shad-form-item'>
+									<FormLabel className='shad-form-label'>Email</FormLabel>
+									<FormControl>
+										<Input
+											placeholder='Enter your email'
+											className='shad-input'
+											{...field}
+										/>
+									</FormControl>
+								</div>
+								<FormMessage className='shad-form-message' />
+							</FormItem>
+						)}
+					/>
+					<Button
+						type='submit'
+						className='form-submit-button ease-transition'
+						disabled={isLoading}
+					>
+						{type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+						{isLoading && (
+							<Image
+								src='/icons/loader.svg'
+								alt='loader'
+								width={20}
+								height={20}
+								className='ml-2 animate-spin'
+							/>
+						)}
+					</Button>
+
+					{errorMessage && <p className='error-message'>{errorMessage}</p>}
+
+					<div className='body-2 flex justify-center'>
+						<p className='text-light-100'>
+							{type === 'sign-in'
+								? "Don't have an account? "
+								: 'Already have an account? '}
+							<Link
+								href={type === 'sign-in' ? '/sign-up' : '/sign-in'}
+								className='text-brand-100 hover:text-brand-100/90 ease-transition ml-1 font-medium'
+							>
+								{type === 'sign-in' ? 'Sign Up' : 'Sign In'}
+							</Link>
+						</p>
+					</div>
+				</form>
+			</Form>
+			{accountId && (
+				<OTPModal accountId={accountId} email={form.getValues('email')} />
+			)}
+		</>
+	);
+};
+
+export default AuthForm;
---
+++ lib/appwrite/config.ts
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+	endPointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!, // exclamation mark is used to ensure that the environment variable is not undefined
+	projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
+	databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
+	usersCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION!,
+	filesCollectionId: process.env.NEXT_PUBLIC_APPWRITE_FILES_COLLECTION!,
+	bucketId: process.env.NEXT_PUBLIC_APPWRITE_BUCKET!,
+	secretKey: process.env.NEXT_APPWRITE_SECRET!, // only available for the backend server actions
+};
